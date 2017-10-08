31aff06befe255e16d1b2a88a379f358caa041c7
Aaron Johnson <aaron9johnson@gmail.com>
2017-10-07 18:43:23 -0700

    Sum

---
+++ SumTests/SumTests.m
@@ -0,0 +1,32 @@
+//
+//  SumTests.m
+//  SumTests
+//
+//  Created by Sam Meech-Ward on 2017-10-06.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SumTests : XCTestCase
+
+@end
+
+@implementation SumTests
+
+int sum(int fromN, int toN) {
+    if(fromN !=toN){
+        return fromN + sum(fromN + 1, toN);
+    }
+    return fromN;
+}
+
+- (void)testSumFunction {
+    XCTAssertEqual(sum(1, 1), 1);
+    XCTAssertEqual(sum(1, 2), 3);
+    XCTAssertEqual(sum(3, 7), 25);
+}
+
+
+
+@end
